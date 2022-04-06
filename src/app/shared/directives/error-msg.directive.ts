1b535a799ffc57e6b899bc9f8e0c2fbe40e5479f
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-04-06 13:15:36 -0500

    mostrar ocultar sí tiene error el campo de nombre

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -21,6 +21,14 @@ export class ErrorMsgDirective implements OnInit{
     this.setMensaje();
   }
 
+  @Input() set valido( valor: boolean ){
+    if( valor ){
+      this.htmlElement.nativeElement.classList.add('hidden');
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hidden');
+    }
+  }
+
   constructor( private el: ElementRef<HTMLElement> ) { 
     this.htmlElement = el;
   }
